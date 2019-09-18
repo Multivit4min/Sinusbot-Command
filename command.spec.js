767910a3202e0b13245c761406c2525dd9a049aa
David Kartnaller <david.kartnaller@gmail.com>
2019-09-19 01:50:20 +0200

    add some more tests

--- command.spec.js
+++ command.spec.js
@@ -5,45 +5,146 @@ describe("Command", () => {
 
   /** @type {Sinusbot} */
   let sinusbot = null
+
+  /** @type {jest.Mock} */
+  let mockFn = null
+
+  /** return form createCommand */
+  let testCmd = null
+
+  /** command.js export */
+  let exported = null
+
   const script = fs.readFileSync("./command.js", "utf8")
 
   beforeEach(() => {
     sinusbot = new Sinusbot()
     sinusbot.setScript(script)
     sinusbot.setConfig({ DEBUGLEVEL: 0, NOT_FOUND_MESSAGE: "0" })
+    mockFn = jest.fn()
+    exported = sinusbot.run()
+    testCmd = exported.createCommand("test")
+      .exec(mockFn)
   })
 
-  it("should test basic registration of a command", () => {
-    const mockFn = jest.fn()
-    const exported = sinusbot.run()
-    exported.createCommand("test")
-      .exec(mockFn)
-    sinusbot.event.chat({ text: "!test" })
-    expect(mockFn).toBeCalledTimes(1)
+  describe("General", () => {
+
+    it("should test basic registration of a command", () => {
+      sinusbot.event.chat({ text: "!test" })
+      expect(mockFn).toBeCalledTimes(1)
+    })
+
+    it("should test basic registration of a command with 2 alias", () => {
+      testCmd.alias("test1", "test2")
+      sinusbot.event.chat({ text: "!test" })
+      sinusbot.event.chat({ text: "!test1" })
+      sinusbot.event.chat({ text: "!test2" })
+      expect(mockFn).toBeCalledTimes(3)
+    })
+
+    it("should test a disabled command", () => {
+      const client = Sinusbot.createClient()
+      client.chatMock = jest.fn()
+      testCmd.disable()
+      sinusbot.event.chat({ text: "!test", client: client.buildModule() })
+      expect(mockFn).toBeCalledTimes(0)
+      expect(client.chatMock).toBeCalledTimes(1)
+    })
+
+    it("should test a reenabling of a command", () => {
+      const client = Sinusbot.createClient()
+      client.chatMock = jest.fn()
+      testCmd.disable().enable()
+      sinusbot.event.chat({ text: "!test", client: client.buildModule() })
+      expect(mockFn).toBeCalledTimes(1)
+      expect(client.chatMock).toBeCalledTimes(0)
+    })
+
+    describe("invalid commands", () => {
+      it("should check if a command throws an error on registration with a space in it", () => {
+        expect(() => exported.createCommand("te st")).toThrowError()
+      })
+
+      it("should check if a command throws an error on registration with a carriage return", () => {
+        expect(() => exported.createCommand("te\rst")).toThrowError()
+      })
+
+      it("should check if a command throws an error on registration with a new line", () => {
+        expect(() => exported.createCommand("te\nst")).toThrowError()
+      })
+
+      it("should check if a command throws an error on registration with a tab", () => {
+        expect(() => exported.createCommand("te\tst")).toThrowError()
+      })
+    })
   })
 
-  it("should test basic registration of a command with 2 alias", () => {
-    const mockFn = jest.fn()
-    const exported = sinusbot.run()
-    exported.createCommand("test")
-      .alias("test1", "test2")
-      .exec(mockFn)
-    sinusbot.event.chat({ text: "!test" })
-    sinusbot.event.chat({ text: "!test1" })
-    sinusbot.event.chat({ text: "!test2" })
-    expect(mockFn).toBeCalledTimes(3)
-  })
 
-  it("should test a disabled command", () => {
-    const mockFn = jest.fn()
-    const exported = sinusbot.run()
-    const client = Sinusbot.createClient()
-    client.chatMock = jest.fn()
-    exported.createCommand("test")
-      .disable()
-      .exec(mockFn)
-    sinusbot.event.chat({ text: "!test", client: client.buildModule() })
-    expect(mockFn).toBeCalledTimes(0)
-    expect(client.chatMock).toBeCalledTimes(1)
+  describe("Arguments", () => {
+
+    describe("StringArgument", () => {
+
+      it("should test the basic assignment of a string", () => {
+        testCmd.addArgument(args => args.string.setName("bar"))
+        sinusbot.event.chat({ text: "!test Foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "Foo" })
+      })
+      it("should test the forceUpperCase method", () => {
+        testCmd.addArgument(args => args.string.setName("bar").forceUpperCase())
+        sinusbot.event.chat({ text: "!test Foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "FOO" })
+      })
+      it("should test the forceLowerCase method", () => {
+        testCmd.addArgument(args => args.string.setName("bar").forceLowerCase())
+        sinusbot.event.chat({ text: "!test Foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "foo" })
+      })
+      it("should test the forceLowerCase method", () => {
+        testCmd.addArgument(args => args.string.setName("bar").forceLowerCase())
+        sinusbot.event.chat({ text: "!test Foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "foo" })
+      })
+      it("should test the match regex", () => {
+        testCmd.addArgument(args => args.string.setName("bar").match(/^fOo$/))
+        sinusbot.event.chat({ text: "!test fOo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "fOo" })
+        mockFn.mockClear()
+        sinusbot.event.chat({ text: "!test foo" })
+        expect(mockFn).toBeCalledTimes(0)
+      })
+      it("should test the max length", () => {
+        testCmd.addArgument(args => args.string.setName("bar").max(3))
+        sinusbot.event.chat({ text: "!test foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "foo" })
+        mockFn.mockClear()
+        sinusbot.event.chat({ text: "!test fooo" })
+        expect(mockFn).toBeCalledTimes(0)
+      })
+      it("should test the min length", () => {
+        testCmd.addArgument(args => args.string.setName("bar").min(3))
+        sinusbot.event.chat({ text: "!test foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "foo" })
+        mockFn.mockClear()
+        sinusbot.event.chat({ text: "!test fo" })
+        expect(mockFn).toBeCalledTimes(0)
+      })
+      it("should test the whitelist method", () => {
+        testCmd.addArgument(args => args.string.setName("bar").whitelist(["foo", "bar"]))
+        sinusbot.event.chat({ text: "!test foo" })
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn.mock.calls[0][1]).toEqual({ bar: "foo" })
+        mockFn.mockClear()
+        sinusbot.event.chat({ text: "!test baz" })
+        expect(mockFn).toBeCalledTimes(0)
+      })
+    })
+
   })
 })
